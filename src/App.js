3703f979e99d854c972065e3cf191009d9934b22
Kuddleman <edl1155@gmail.com>
2020-06-16 12:06:33 -0700

    complete refactor to redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
+import reducer from './reducers/'   
+// do I need to indicate index on line 3?
 
 import { createStore } from 'redux'
-const store = createStore( reducer )
+
+const initialState = { tech: "React" }
+const store = createStore( reducer, initialState )
 
 class App extends Component {
   
  
   render() {
-    return <HelloWorld tech={this.state.tech} />;
+    return <HelloWorld tech={ store.getState().tech } />;
   }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
 export default ( state ) => {
 
+    return state
 }
