0037de9a020acf5d200c1be4f43372fe9860d0a0
Kuddleman <edl1155@gmail.com>
2020-06-16 11:40:38 -0700

    remove state from app and add redux in CL

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
 
 class App extends Component {
-  state = {
-    tech: "Redux"
-  };
+  
   render() {
     return <HelloWorld tech={this.state.tech} />;
   }
