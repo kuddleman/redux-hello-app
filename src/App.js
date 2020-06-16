a00013cd307090700e4d9106997776133475d67d
Kuddleman <edl1155@gmail.com>
2020-06-16 11:45:17 -0700

    import create store and declare store variable in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
 
+import { createStore } from 'redux'
+const store = createStore( reducer )
+
 class App extends Component {
   
+ 
   render() {
     return <HelloWorld tech={this.state.tech} />;
   }
